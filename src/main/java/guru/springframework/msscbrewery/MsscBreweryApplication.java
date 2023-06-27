561eb5ffe6d10a2a60c58f00037e52c1307d299a
Valeri Totev <valeri.totev@blankfactor.com>
2023-06-27 14:19:55 +0300

    created dto, service and controller

--- src/main/java/guru/springframework/msscbrewery/MsscBreweryApplication.java
+++ src/main/java/guru/springframework/msscbrewery/MsscBreweryApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//		(scanBasePackages =
+//		{"guru.springframework.msscbrewery.services",
+//		"guru.springframework.msscbrewery.web",
+//		"guru.springframework.msscbrewery.web.controller"})
 public class MsscBreweryApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/guru/springframework/msscbrewery/services/BeerServiceImpl.java
@@ -0,0 +1,18 @@
+package guru.springframework.msscbrewery.services;
+
+import guru.springframework.msscbrewery.web.model.BeerDto;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class BeerServiceImpl implements BeerService {
+    @Override
+    public BeerDto getBeerById(UUID beerId) {
+        return BeerDto.builder()
+                .id(UUID.randomUUID())
+                .beerName("Galaxy Cat")
+                .beerStyle("Pale Ale")
+                .build();
+    }
+}
