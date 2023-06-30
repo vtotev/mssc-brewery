df6332326def380ba51de60782043309b5be8723
Valeri Totev <valeri.totev@blankfactor.com>
2023-06-30 10:22:26 +0300

    added post and put methods in beet service

--- src/main/java/guru/springframework/msscbrewery/services/BeerService.java
+++ src/main/java/guru/springframework/msscbrewery/services/BeerService.java
@@ -6,4 +6,8 @@ import java.util.UUID;
 
 public interface BeerService {
     BeerDto getBeerById(UUID beerId);
+
+    BeerDto saveNewBeer(BeerDto beerDto);
+
+    void updateBeer(UUID beerId, BeerDto beerDto);
 }
--- src/main/java/guru/springframework/msscbrewery/services/BeerServiceImpl.java
+++ src/main/java/guru/springframework/msscbrewery/services/BeerServiceImpl.java
@@ -15,4 +15,16 @@ public class BeerServiceImpl implements BeerService {
                 .beerStyle("Pale Ale")
                 .build();
     }
+
+    @Override
+    public BeerDto saveNewBeer(BeerDto beerDto) {
+        return BeerDto.builder()
+                .id(UUID.randomUUID())
+                .build();
+    }
+
+    @Override
+    public void updateBeer(UUID beerId, BeerDto beerDto) {
+        // todo impl - would add a real impl to update beer
+    }
 }
